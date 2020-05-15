89719a3c02662bea76533b8f2bb99c94727d8bb8
Monehin Emmanuel <e.monehin@live.com>
2020-05-15 14:37:16 +0200

    setup CRA with tailwind

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles/tailwind.generated.css';
+
+function App() {
+  return <div className='bg-blue-300 w-full h-screen'>Hello World</div>;
+}
+
+export default App;
