5c756efdf320286907fc04c66a6daffd98d81b48
Monehin Emmanuel <e.monehin@live.com>
2020-05-16 00:11:57 +0200

    add basic content to home page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from 'react';
 import './styles/tailwind.generated.css';
 
 function App() {
-  return <div className='bg-blue-300 w-full h-screen'>Hello World</div>;
+  return (
+    <div className='bg-red-300 flex w-full h-screen items-center'>
+      <div className='px-10 flex flex-col'>
+        <h4 className='tracking-widest text-2xl font-thin'>Hi, I'm</h4>
+        <h1 className='tracking-widest text-6xl font-medium'>
+          Emmanuel Monehin
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
