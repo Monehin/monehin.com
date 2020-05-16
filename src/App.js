1b7cf5de356f707705b874a87ef7be6ddaa270d8
Monehin Emmanuel <e.monehin@live.com>
2020-05-16 16:30:51 +0200

    added homepage text

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './styles/tailwind.generated.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className='bg-red-300 flex w-full h-screen items-center'>
-      <div className='px-10 flex flex-col'>
-        <h4 className='tracking-widest text-2xl font-thin'>Hi, I'm</h4>
-        <h1 className='tracking-widest text-6xl font-medium'>
-          Emmanuel Monehin
-        </h1>
-      </div>
+      <HomePage />
     </div>
   );
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 const Homepage = () => {
   return (
-    <div>
-      <div></div>
+    <div className='px-10 flex flex-col'>
+      <h4 className='tracking-widest md:text-xl sm:text-sm font-thin pl-4'>
+        Hi, I'm
+      </h4>
+      <h1 className='tracking-widest text-xl sm:text-4xl md:text-5xl lg:text-6xl font-bold'>
+        Emmanuel Monehin
+      </h1>
+      <p className='tracking-wide md:text-xl sm:text-sm'>
+        I write code, think design, and create growth & development experiences
+        for teams.
+        <br /> <br />
+      </p>
+      <p className='tracking-wide md:text-xl sm:text-sm'>
+        Currently Senior Engineering Program Coordinator at&nbsp;
+        <a className='font-semibold' href='https://andela.com/'>
+          Andela
+        </a>
+      </p>
     </div>
   );
 };
