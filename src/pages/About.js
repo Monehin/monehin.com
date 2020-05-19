ca63b951b81b12aa489b3b1bfd4a3a506e6098f2
Monehin Emmanuel <e.monehin@live.com>
2020-05-19 09:50:16 +0200

    about me content struture

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,12 +3,12 @@ import React from 'react';
 const About = () => {
   return (
     <section className='w-full h-screen flex'>
-      <div className='about-title h-full flex w-1/2 justify-center items-center font-extrabold text-4xl '>
+      <div className='tracking-widest about-title h-full flex w-1/2 justify-center items-center font-extrabold text-4xl '>
         About Me
       </div>
       <div className='h-screen w-1/2 overflow-y-scroll '>
         <div className='flex flex-col justify-around h-screen items-center h-1/2'>
-          <h1 className=''>Project One</h1>
+          <h1 className='tracking-wide font-bold text-3xl'>Background</h1>
           <p className=''>
             Quis in id irure ad in. Consectetur dolor et do dolore do eiusmod
             cupidatat tempor ea culpa proident dolor. Nisi sit enim velit aute.
@@ -22,7 +22,9 @@ const About = () => {
           </p>
         </div>
         <div className='flex flex-col justify-around h-screen items-center'>
-          <h1 className=''>Project Two</h1>
+          <h1 className='tracking-wide font-bold text-3xl'>
+            Skills & Competences
+          </h1>
           <p className=''>
             Quis in id irure ad in. Consectetur dolor et do dolore do eiusmod
             cupidatat tempor ea culpa proident dolor. Nisi sit enim velit aute.
@@ -33,7 +35,7 @@ const About = () => {
           </p>
         </div>
         <div className='flex flex-col justify-around h-screen items-center'>
-          <h1 className=''>Project Three</h1>
+          <h1 className='tracking-wide font-bold text-3xl'>Projects</h1>
           <p className=''>
             Quis in id irure ad in. Consectetur dolor et do dolore do eiusmod
             cupidatat tempor ea culpa proident dolor. Nisi sit enim velit aute.
