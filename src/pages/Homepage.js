56d45ff1d12071ad18eb5d752dc82c88b95e0eed
Monehin Emmanuel <e.monehin@live.com>
2020-05-19 10:28:31 +0200

    update home content

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -11,8 +11,12 @@ const Homepage = () => {
           >
             Hi, I'm
           </h4>
-          <h1 className='py-5'>
-            <Name />
+          <h1
+            id='my-name'
+            className='tracking-widest text-xl sm:text-4xl md:text-6xl lg:text-7xl font-bold'
+          >
+            {/* <Name /> */}
+            Emmanuel Monehin
           </h1>
           <p
             id='basic-info'
@@ -22,15 +26,26 @@ const Homepage = () => {
             <br /> <br />
           </p>
           <p
-            id='current-job-text'
+            id='what-i-do-text'
             className='tracking-wide md:text-xl sm:text-sm font-semibold'
           >
-            Currently Senior Engineering Program Coordinator at&nbsp;
-            <a className='font-semibold' href='https://andela.com/'>
-              Andela
-            </a>
+            and create growth and development experiences with Engineering teams
           </p>
         </div>
+      </div>
+      <div className='flex px-10 py-5'>
+        <p
+          id='current-job-text'
+          className='tracking-wide md:text-xl sm:text-sm font-semibold'
+        >
+          Currently Senior Engineering Program Coordinator at&nbsp;
+          <a
+            className='font-semibold text-purple-900 md:text-3xl '
+            href='https://andela.com/'
+          >
+            Andela
+          </a>
+        </p>
       </div>
     </section>
   );
