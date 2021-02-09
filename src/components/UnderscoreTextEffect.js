6cdd47576f129b06b1968bb93941fee7b1329e22
Monehin Emmanuel <e.monehin@live.com>
2021-02-09 05:43:49 +0200

    Update UnderscoreTextEffect.js

--- src/components/UnderscoreTextEffect.js
+++ src/components/UnderscoreTextEffect.js
@@ -3,7 +3,7 @@ import { TweenMax } from 'gsap';
 
 const UnderscoreTextEffect = () => {
   let underscore = useRef(null);
-  const skills = ['Frontend Engineering', 'UI / UX', 'Product Management'];
+  const skills = ['Scrum Master', 'Frontend Engineering', 'UI / UX', 'Product Management'];
   const colors = ['orange', 'rebeccapurple', 'green'];
   const [skillText, setSkillText] = useState('');
   const [color, setColor] = useState(colors[0]);
